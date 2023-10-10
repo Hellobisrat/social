bbdfa63ea2efc720c9d0e774cd16870d8965729f
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2023-10-10 17:04:04 -0500

    update post

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,12 @@ const Post = require('../models/Post')
 
 
 
+
+// get a post
+router.get('/', async(req,res)=>{
+  const post =  await Post.find()
+  res.send(post)
+})
 // create a post
 
 router.post('/', async(req,res)=>{
@@ -22,17 +28,15 @@ router.post('/', async(req,res)=>{
 router.put('/:id', async(req,res) =>{
   try {
     
-    const post = await Post.findById(req.params.id)
-    if(post.userId === req.body.userId){
+    const post = await Post.findById(req.params.userId)
+    console.log(post)
+  
    const updatePost =  await post.updateOne({$set:req.body})
       res.status(200).json('the post has been updated')
-    }
-    else {
-      res.status(403).json('you can update only your own post')
-    }
    
     
   } catch (error) {
+    console.log(error)
     res.status(500).json(error)
   }
  
